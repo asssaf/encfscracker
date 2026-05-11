b80ba65d6c5e731dfd478a08222ab1b567b024c8
assaf <asssaf@users.noreply.github.com>
2026-05-11 06:46:53 +0000

    fix(conductor): Fix build error in combination engine

--- src/fragment_combination/mod.rs
+++ src/fragment_combination/mod.rs
@@ -1,32 +1,32 @@
-use std::rc::Rc;
+use std::sync::Arc;
 
 pub mod parallel;
 
-pub fn generate_combinations<T: Clone + 'static>(fragments: &[T], k: usize) -> impl Iterator<Item = Vec<T>> {
-    let fragments = Rc::new(fragments.to_vec());
+pub fn generate_combinations<T: Clone + Send + Sync + 'static>(fragments: &[T], k: usize) -> Box<dyn Iterator<Item = Vec<T>> + Send> {
+    let fragments = Arc::new(fragments.to_vec());
     let n = fragments.len();
     
     if k == 0 {
-        return Box::new(std::iter::once(vec![])) as Box<dyn Iterator<Item = Vec<T>>>;
+        return Box::new(std::iter::once(vec![]));
     }
     if k > n {
-        return Box::new(std::iter::empty()) as Box<dyn Iterator<Item = Vec<T>>>;
+        return Box::new(std::iter::empty());
     }
     
     Box::new(
         (0..n).flat_map(move |i| {
             let mut rest = (*fragments).clone();
             rest.remove(i);
-            let fragments = Rc::clone(&fragments);
+            let fragments = Arc::clone(&fragments);
             
             if k == 1 {
-                Box::new(std::iter::once(vec![fragments[i].clone()])) as Box<dyn Iterator<Item = Vec<T>>>
+                Box::new(std::iter::once(vec![fragments[i].clone()])) as Box<dyn Iterator<Item = Vec<T>> + Send>
             } else {
                 Box::new(generate_combinations(&rest, k - 1).map(move |c| {
                     let mut res = vec![fragments[i].clone()];
                     res.extend(c);
                     res
-                }))
+                })) as Box<dyn Iterator<Item = Vec<T>> + Send>
             }
         })
     )
